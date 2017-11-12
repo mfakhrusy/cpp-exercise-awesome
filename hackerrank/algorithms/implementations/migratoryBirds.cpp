b30d91e926d96bbf949599380975f236056df8d3
Muhamad Fakhrusy <v8areu@gmail.com>
2017-11-12 14:22:56 +0700

    add new solutions

--- hackerrank/algorithms/implementations/migratoryBirds.cpp
+++ hackerrank/algorithms/implementations/migratoryBirds.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <array>
 
 std::vector<int> strToVector(std::string, int);
 
@@ -14,7 +15,42 @@ int main() {
 	std::getline(std::cin, stringBirdsType);
 	std::vector<int> vecBirdsType = strToVector(stringBirdsType, birdsNumber);
 	
-	for ()
+	//int countType1, countType2, countType3, countType4, countType5;
+	std::array<int, 5> countBirdType = {0, 0, 0, 0, 0};
+	for (auto birdsType : vecBirdsType) {
+		
+		switch(birdsType) {
+			case 1:
+				countBirdType[0]++;
+				break;
+			case 2:
+				countBirdType[1]++;
+				break;
+			case 3:
+				countBirdType[2]++;
+				break;
+			case 4:
+				countBirdType[3]++;
+				break;
+			case 5:
+				countBirdType[4]++;
+				break;
+			default:
+				std::cout << "Bird type is wrong!\n";
+		}
+	}
+
+	// check maximum
+	int mostType = 1;
+	int tempMostBirdType = countBirdType[0];
+	for (auto i = 0; i < countBirdType.size(); i++) {
+		if (countBirdType[i] > tempMostBirdType) {
+			mostType = i + 1;
+			tempMostBirdType = countBirdType[i];
+		}
+	}
+
+	std::cout << mostType << std::endl;
 	return 0;
 }
 
