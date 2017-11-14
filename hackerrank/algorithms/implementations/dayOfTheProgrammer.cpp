ee7290b56b85e07115e2ee918cde6a1726e005c3
Muhamad Fakhrusy <v8areu@gmail.com>
2017-11-15 05:15:52 +0700

    finish day ot the programmer

--- hackerrank/algorithms/implementations/dayOfTheProgrammer.cpp
+++ hackerrank/algorithms/implementations/dayOfTheProgrammer.cpp
@@ -9,7 +9,7 @@ class Calendar {
 		int day;
 
 		void calcDate(Calendar &, int &, int, bool &);
-
+		void printDate(Calendar);
 };
 
 int main() {
@@ -19,11 +19,12 @@ int main() {
 
 	// calculate date
 	prog.day = 0;
-	prog.month = 0;
+	prog.month = 1;
 
 	int tempDay = PROGRAMMER_DAY;
 	bool checkMonth = true;
 	for (auto countMonth = 1; countMonth <= 12; countMonth++) {
+		//std::cout << tempDay << ":" << countMonth << std::endl;
 		
 		if (!checkMonth) {
 			break;
@@ -39,14 +40,16 @@ int main() {
 			case 12:
 				monthlyDay = 31;
 				prog.calcDate(prog, tempDay, monthlyDay, checkMonth);
+				break;
 			case 4:
 			case 6:
 			case 9:
 			case 11:
 				monthlyDay = 30;
 				prog.calcDate(prog, tempDay, monthlyDay, checkMonth);
+				break;
 			case 2:
-				if (prog.year >= 1700 || prog.year <= 1917) {
+				if (prog.year >= 1700 && prog.year <= 1917) {
 
 					if (prog.year % 4 == 0) {
 						monthlyDay = 29;
@@ -58,18 +61,23 @@ int main() {
 
 					monthlyDay = 15; // because it started at 14th february
 				
-				} else if (prog.year >= 1919 || prog.year <= 2700) {
+				} else if (prog.year >= 1919 && prog.year <= 2700) {
+
 					if (prog.year % 400 == 0 || (prog.year % 4 == 0 && prog.year % 100 != 0)) {
 						monthlyDay = 29;
 					} else {
 						monthlyDay = 28;
 					}
+
 				} else {
 					std::cout << "wrong!\n";
 				}
 				prog.calcDate(prog, tempDay, monthlyDay, checkMonth);
+				break;
 		}
 	}
+
+	prog.printDate(prog);
 
 	return 0;
 }
@@ -85,4 +93,28 @@ void Calendar::calcDate(Calendar &prog, int &tempDay,
 		prog.month += 1;
 	}
 
+}
+
+// print function for easiness
+void Calendar::printDate(Calendar prog) {
+
+	std::string printDay;
+	std::string printMonth;
+	std::string printYear;
+
+	if (prog.day < 10) {
+		printDay = "0" + std::to_string(prog.day);
+	} else {
+		printDay = std::to_string(prog.day);
+	}
+
+	if (prog.month < 10) {
+		printMonth = "0" + std::to_string(prog.month);
+	} else {
+		printMonth = std::to_string(prog.month);
+	}
+
+	printYear = std::to_string(prog.year);
+
+	std::cout << printDay << "." << printMonth << "." << printYear << std::endl;
 }
