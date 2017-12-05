cdcf6212c037b6d65618f9ea085787ec00249767
Muhamad Fakhrusy <v8areu@gmail.com>
2017-12-06 06:16:27 +0700

    I just wanted to be with a to-be wanted thing

---
+++ hackerrank/sorting/bigSorting.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+
+	// input first row (number of big ints
+	unsigned int num;
+	(std::cin >> num).get();
+
+	// input the rest
+	std::vector<std::string> sortedBigNum(num);
+	for (std::string &numString : sortedBigNum) {
+
+		std::string tempNum;
+		std::getline(std::cin, tempNum);
+
+
+		
+	}
+
+}
